57b2b9fd44835a9f009a9b8601df04dcb522f9e3
Mispon <tomythesupa@gmail.com>
2021-06-01 11:05:01 +0300

    removed New() method from model and change tests

--- internal/models/runner.go
+++ internal/models/runner.go
@@ -2,43 +2,30 @@ package models
 
 import (
 	"fmt"
-	"github.com/google/uuid"
-	"runtime"
 )
 
 type Runner struct {
-	guid string
-	os   string
-	arch string
-}
-
-// New creates new Runner instance
-func New(guid string, os string, arch string) *Runner {
-	return &Runner{guid, os, arch}
-}
-
-// NewDefault creates new Runner instance with system values
-func NewDefault() *Runner {
-	guid, _ := uuid.NewUUID()
-	return &Runner{guid.String(), runtime.GOOS, runtime.GOARCH}
+	Guid string
+	Os   string
+	Arch string
 }
 
 // GetGuid returns current runner guid
 func (runner *Runner) GetGuid() string {
-	return runner.guid
+	return runner.Guid
 }
 
 // GetOs returns current runner os
 func (runner *Runner) GetOs() string {
-	return runner.os
+	return runner.Os
 }
 
 // GetArch returns current runner arch
 func (runner *Runner) GetArch() string {
-	return runner.arch
+	return runner.Arch
 }
 
 // String returns formatted info
 func (runner *Runner) String() string {
-	return fmt.Sprintf("Runner (guid: %v, os: %v, arch: %v)", runner.guid, runner.os, runner.arch)
+	return fmt.Sprintf("Runner (guid: %v, os: %v, arch: %v)", runner.Guid, runner.Os, runner.Arch)
 }
--- internal/models/runner_test.go
+++ internal/models/runner_test.go
@@ -1,26 +1,17 @@
 package models
 
 import (
-	"runtime"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInit(t *testing.T) {
+func TestGetters(t *testing.T) {
 	guid, _ := uuid.NewUUID()
-	runner := New(guid.String(), "windows", "x64")
+	runner := Runner{guid.String(), "windows", "x64"}
 
 	assert.Equal(t, guid.String(), runner.GetGuid())
 	assert.Equal(t, "windows", runner.GetOs())
 	assert.Equal(t, "x64", runner.GetArch())
-}
-
-func TestInitDefaults(t *testing.T) {
-	runner := NewDefault()
-
-	assert.NotEmpty(t, runner.GetGuid())
-	assert.Equal(t, runtime.GOOS, runner.GetOs())
-	assert.Equal(t, runtime.GOARCH, runner.GetArch())
 }
